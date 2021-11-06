e6cc77fc45827014719ecfe5862f14ae2728f352
QOA <58850207+QAKANDE@users.noreply.github.com>
2021-11-06 18:44:20 +0000

    most recent

--- src/Services/index.js
+++ src/Services/index.js
@@ -1,94 +1,134 @@
-const express = require('express')
-const router = express.Router()
-const servicesModel = require('../Services/schema')
+const express = require("express");
+const router = express.Router();
+const servicesModel = require("../Services/schema");
 
-router.get('/', async(req, res) => {
-    try {
-        const services = await servicesModel.find()
-        res.send(services)
-    } catch (error) {
-        console.log(error)
+router.get("/", async (req, res) => {
+  try {
+    const services = await servicesModel.find();
+    res.send(services);
+  } catch (error) {
+    console.log(error);
+  }
+});
+router.get("/get-by-service/:id", async (req, res) => {
+  try {
+    const serviceFound = await servicesModel.findById(req.params.id);
+    res.send(serviceFound);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+router.get("/get-all-images", async (req, res) => {
+  const imagesUpdate = [];
+  try {
+    const fullGlam = await servicesModel.findById("6186bada7ec60043136b45d1");
+    const naturalGlam = await servicesModel.findById(
+      "6186b835ae82573f46d767ba"
+    );
+    const softGlam = await servicesModel.findById("6186bba27ec60043136b45dc");
+
+    fullGlam.images.map((im) => {
+      imagesUpdate.push({ serviceName: "Full glam", url: im.url });
+    });
+    naturalGlam.images.map((im) => {
+      imagesUpdate.push({ serviceName: "Natural glam", url: im.url });
+    });
+    softGlam.images.map((im) => {
+      imagesUpdate.push({ serviceName: "Soft glam", url: im.url });
+    });
+
+    res.send({
+      data: imagesUpdate,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+router.post("/new-service", async (req, res) => {
+  try {
+    const { url, serviceName, price, pricePerHour, desc, features } = req.body;
+    const newFeatureArr = [];
+    const featureArr = features.split(",");
+    featureArr.map((feat) => {
+      newFeatureArr.push({ text: feat });
+    });
+    let allServices = await servicesModel.find();
+    const serviceFound = allServices.filter(
+      (service) => service.serviceName === serviceName
+    );
+    if (serviceFound.length === 0) {
+      const newService = await servicesModel.create({
+        serviceName,
+        price,
+        pricePerHour,
+        desc,
+        features: newFeatureArr,
+        images: [{ url }],
+      });
+      return res.status(201).send(newService);
+    } else {
+      const serviceId = serviceFound[0]._id;
+      let serviceById = await servicesModel.findById(serviceId);
+      if (serviceById && serviceFound.length !== 0) {
+        serviceById.images.push({ url });
+        serviceById = await serviceById.save();
+        res.send("New service image added");
+      }
     }
-})
-router.get('/get-by-service/:id', async(req, res) => {
-    try {
-        const serviceFound = await servicesModel.findById(req.params.id)
-        res.send(serviceFound)
-    } catch (error) {
-        console.log(error)
-    }
-})
+    // if (allServices.length === 0) {
+    //   const newService = await servicesModel.create({
+    //     serviceName,
+    //     price,
+    //     pricePerHour,
+    //     desc,
+    //     features: newFeatureArr,
+    //     images: [{ url }],
+    //   });
+    //   return res.status(201).send(newService);
+    // } else {
+    //   const serviceFound = allServices.filter(
+    //     (service) => service.serviceName === serviceName
+    //   );
+    //   const serviceId = serviceFound[0]._id;
+    //   let serviceById = await servicesModel.findById(serviceId);
+    //   if (serviceById && serviceFound.length !== 0) {
+    //     serviceById.images.push({ url });
+    //     serviceById = await serviceById.save();
+    //     res.send("New service image added");
+    //   }
+    // }
 
-router.get('/get-all-images', async(req, res) => {
-    const imagesUpdate = []
-    try {
-        const fullGlam = await servicesModel.findById('60854b62cd35326768b6af78')
-        const naturalGlam = await servicesModel.findById('60854ac4cd35326768b6af6d')
-        const softGlam = await servicesModel.findById('60854b72cd35326768b6af83')
+    // if (serviceById && serviceFound.length !== 0) {
+    //   serviceById.images.push({ url });
+    //   serviceById = await serviceById.save();
+    //   res.send("New service image added");
+    // } else {
+    //   const newService = await servicesModel.create({
+    //     serviceName,
+    //     price,
+    //     pricePerHour,
+    //     desc,
+    //     features: newFeatureArr,
+    //     images: [{ url }],
+    //   });
+    //   return res.status(201).send(newService);
+    // }
+  } catch (error) {
+    console.log(error);
+  }
+});
 
-        fullGlam.images.map((im) => {
-            imagesUpdate.push({ serviceName: 'Full glam', url: im.url })
-        })
-        naturalGlam.images.map((im) => {
-            imagesUpdate.push({ serviceName: 'Natural glam', url: im.url })
-        })
-        softGlam.images.map((im) => {
-            imagesUpdate.push({ serviceName: 'Soft glam', url: im.url })
-        })
+router.put("/edit-service/:id", async (req, res) => {
+  try {
+    const servicesToBeEdited = await servicesModel.findByIdAndUpdate(
+      req.params.id
+    );
+    res.send(servicesModel);
+  } catch (error) {
+    console.log(error);
+  }
+});
 
-        res.send({
-            data: imagesUpdate,
-        })
-    } catch (error) {
-        console.log(error)
-    }
-})
-
-router.post('/new-service', async(req, res) => {
-    try {
-        const { url, serviceName, price, pricePerHour, desc, feature } = req.body
-        const newFeatureArr = []
-        const featureArr = feature.split(',')
-        featureArr.map((feat) => {
-            newFeatureArr.push({ text: feat })
-        })
-
-        let allServices = await servicesModel.find()
-        const serviceFound = allServices.filter(
-            (service) => service.serviceName === serviceName,
-        )
-        const serviceId = serviceFound[0]._id
-        let serviceById = await servicesModel.findById(serviceId)
-
-        if (serviceById && serviceFound.length !== 0) {
-            serviceById.images.push({ url })
-            serviceById = await serviceById.save()
-            res.send('New service image added')
-        } else {
-            const newService = await servicesModel.create({
-                serviceName,
-                price,
-                pricePerHour,
-                desc,
-                features: newFeatureArr,
-                images: [{ url }],
-            })
-            return res.status(201).send(newService)
-        }
-    } catch (error) {
-        console.log(error)
-    }
-})
-
-router.put('/edit-service/:id', async(req, res) => {
-    try {
-        const servicesToBeEdited = await servicesModel.findByIdAndUpdate(
-            req.params.id,
-        )
-        res.send(servicesModel)
-    } catch (error) {
-        console.log(error)
-    }
-})
-
-module.exports = router
+module.exports = router;
--- src/server.js
+++ src/server.js
@@ -1,60 +1,63 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const cors = require('cors')
-const logger = require('morgan')
-const cookieParser = require('cookie-parser')
-const bodyParser = require('body-parser')
-const services = require('../src/Services/index')
-const appointments = require('../src/Appointments/index')
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const logger = require("morgan");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const services = require("../src/Services/index");
+const appointments = require("../src/Appointments/index");
 
-const server = express()
+const server = express();
 
-const environment = server.get('env')
+const environment = server.get("env");
 
-const port = process.env.PORT || 3003
+const port = process.env.PORT || 3003;
 
-server.use(cors())
-server.use(express.json())
-server.use(logger('dev'))
-server.use(bodyParser.json())
+server.use(cors());
+server.use(express.json());
+server.use(logger("dev"));
+server.use(bodyParser.json());
 server.use(
-    bodyParser.urlencoded({
-        extended: false,
-    }),
-)
-server.use(cookieParser())
-server.use('/services', services)
-server.use('/appointments', appointments)
+  bodyParser.urlencoded({
+    extended: false,
+  })
+);
+server.use(cookieParser());
+server.use("/services", services);
+server.use("/appointments", appointments);
 
-if (environment === 'production') {
-    server.use(function(err, req, res, next) {
-        res.status(err.status || 500)
-        res.render('error', {
-            message: err.message,
-            error: {},
-        })
-    })
+if (environment === "production") {
+  server.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.render("error", {
+      message: err.message,
+      error: {},
+    });
+  });
 } else {
-    // development error handler
-    // will print stacktrace
-    server.use(function(err, req, res, next) {
-        res.status(err.status || 500)
-        res.render('error', {
-            message: err.message,
-            error: err,
-        })
-    })
+  // development error handler
+  // will print stacktrace
+  server.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.render("error", {
+      message: err.message,
+      error: err,
+    });
+  });
 }
 
 mongoose
-    .connect('mongodb://localhost:27017/bisbeautyuk', {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useFindAndModify: false,
+  .connect(
+    `mongodb+srv://bisbeauty:${process.env.MONGOATLASPASSWORD}@cluster0.lrljy.mongodb.net/${process.env.MONGOATLASDATABASENAME}?retryWrites=true&w=majority`,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+    }
+  )
+  .then(
+    server.listen(port, () => {
+      console.log("Server is running on port", port);
     })
-    .then(
-        server.listen(port, () => {
-            console.log('Server is running on port', port)
-        }),
-    )
-    .catch((err) => console.log(err))
+  )
+  .catch((err) => console.log(err));
